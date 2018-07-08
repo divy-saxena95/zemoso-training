598d8042880c2c2e303c337b02bb82779af3000d
divy-saxena95 <35424683+divy-saxena95@users.noreply.github.com>
2018-07-08 14:54:33 +0530

    Add files via upload

--- kycrange.java
+++ kycrange.java
@@ -1,77 +1,84 @@
 import java.io.*;
 import java.text.*;
 import java.util.*;
-public class kycrange{
-private Date signupdate;
-private Date currentdate;
-private final SimpleDateFormat dateform =new SimpleDateFormat("dd-MM-yyyy");
-public kycrange(String sign,String curr)  {
+ class kyc{
+    private Date signup;
+    private Date current;
+    private final SimpleDateFormat dateform =new SimpleDateFormat("dd-MM-yyyy");
+    public kyc(String signupdate,String currentdate)  {
         try {
-            sign= dateform.parse(signupdate);
-            curr = dateform.parse(currentdate);
+            signup= dateform.parse(signupdate);
+            current = dateform.parse(currentdate);
         } catch (ParseException e) {
             e.printStackTrace();
         }
     }
-public boolean validity() {
+    public boolean validity() {
 
-        return sign.compareTo(curr) <= 0;
-             
-   }
-public Date AnniversaryDate() {
+        return signup.compareTo(current) <= 0;
+
+    }
+    public Date AnniversaryDate() {
 
         Calendar ani = Calendar.getInstance();
-        ani.setTime(sign);
+        ani.setTime(signup);
 
         Calendar cur = Calendar.getInstance();
-        cur.setTime(curr);
+        cur.setTime(current);
         cur.add(Calendar.DATE,30);
-        Date i1=cur.getTime();
-        Date i2=ani.getTime();
         ani.set(Calendar.YEAR,cur.get(Calendar.YEAR));
         if(ani.after(cur)){
             ani.add(Calendar.YEAR,-1);
         }
         Date anniversary=ani.getTime();
-        return anniversary;//anni is used to convert calender to Date which is return type
+        return anniversary;
 
     }
 
-public String generateFormDateRange(Date anniv) {
+    public String generateRange(Date anniv) {
 
-        Calendar cal = Calendar.getInstance();       
-         cal.setTime(anniv);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(anniv);
         cal.add(Calendar.DATE, -30);
         Date startani = cal.getTime();
         cal.add(Calendar.DATE, 60);
         Date endani = cal.getTime();
-        if(endani.after(curr)) {
-            endani=curr;
+        if(endani.after(current)) {
+            endani=current;
         }
-        if(startani.before(sign)){
-            startani=sign;
+        if(startani.before(signup)){
+            startani=signup;
         }
         return (dateform.format(startani)+" to "+dateform.format(endani));}}
-    
- 
-public static void main(String [] args)
-{
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));Scanner input=new Scanner(System.in);
-        String s=null;
-        System.out.println("Enter signup date and current date in \'dd-mm-yyyy\' dateform separated by space");
-        s=br.readLine();
-        
-            String[] dates = s.split(" ");
-            kycrange form = new kycrange(dates[0], dates[1]);
-            if (form.validity()) {
-                Date anniv = form.AnniversaryDate();
-                System.out.println("You can file your KYC for dates: " + form.generateFormDateRange(anniv));
-            } else {
-                System.out.println("You cannot file the KYC for the given dates");
-            }
+public class kycrange{
+public static void main(String [] args)throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    Scanner input = new Scanner(System.in);
+    String s = null;int i,n;
+System.out.println("Enter the number of inputs");
+n = Integer.parseInt(input.next());
+for(i=1;i<=n;i++){
+     
+    System.out.println("Enter signup date and current date in \'dd-mm-yyyy\' dateform separated by space");
+    if (args.length == 2)
+        s = args[0] + " " + args[1];
+    else
+        s = br.readLine();
+    
+        String[] dates = s.split(" ");
+       
+        kyc form = new kyc(dates[0], dates[1]);
+        if (form.validity()) {
+            Date anniv = form.AnniversaryDate();
+            System.out.println("You can file your KYC for dates: " + form.generateRange(anniv));
+        } else {
+            System.out.println("No Range");
         }
-        
-    }
+}
+}}
+
+
 
 
